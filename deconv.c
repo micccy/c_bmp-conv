0c2502605616bf588dc183bc08ad188881a11acd
micccy <michele.giacomone@live.com>
2023-02-23 16:53:19 +0100

    Finished preliminary parser

--- deconv.c
+++ deconv.c
@@ -33,7 +33,13 @@ struct __attribute((__packed__)) h_def{
 uint8_t *pix;
 
 FILE *input;
-uint32_t insize;
+uint32_t temp,t0,insize;
+
+const char magind[][4]={"B\0\0", "KiB", "MiB", "GiB"};
+struct __attribute((__packed__)) s_def{
+    uint16_t mag   :  6;                                                    //Struct for typing out file sizes
+    uint16_t value : 10;
+} prsiz;
 
 int main(int argc,char*argv[]){
     if(argc==2){
@@ -49,15 +55,42 @@ int main(int argc,char*argv[]){
                 if(insize==fread(buffer,1,insize,input)){                                                   //Read file into buffer
                     fclose(input);                                                                          //Close file
                     header=(struct h_def*)buffer;                                                           //Set header pointer
-
                     if((header->height > 0)&&(header->planes==1)&&
                     (((header->compr==BI_RGB)&&(header->hsize>=24))||((header->compr==BI_BITFIELDS)&&
                     (header->hsize>=52)))){                                                                 //Check if a sane file has been opened
+                        prsiz.mag=0;
+                        temp=header->fsize;
+                        while(temp>1023){temp/=1024;prsiz.mag++;};
+                        prsiz.value=temp;
+                        printf("\'%s\' %u%s, ",argv[1], prsiz.value, magind[prsiz.mag]);                    //Print name and size of opened file
+                        printf("%ix%i, %ubpp, ",header->width, header->height, header->bpp);                 //Print dimensions and bits per pixel
+                        if(header->compr){                                                                  //If bitfields are specified, print them with RGBAX syntax
+                            temp=0;
+                            t0=32;
+                            for(int i=0;i<32;i++) temp+=((header->rmsk&(1<<i))?1:0);
+                            printf("%u.", temp);
+                            t0-=temp;
+                            temp=0;
+                            for(int i=0;i<32;i++) temp+=((header->gmsk&(1<<i))?1:0);
+                            printf("%u.", temp);
+                            t0-=temp;
+                            temp=0;
+                            for(int i=0;i<32;i++) temp+=((header->bmsk&(1<<i))?1:0);
+                            printf("%u.", temp);
+                            t0-=temp;
+                            temp=0;
+                            for(int i=0;i<32;i++) temp+=((header->amsk&(1<<i))?1:0);
+                            t0-=temp;
+                            printf("%u.%u, ", temp,t0);
+                        }
+                        if(header->ncol) printf("%ucol, ", header->ncol);                                   //If not inferred from the bpp, print the number of colors
+                        if(header->hres!=header->vres) printf("%ix", (header->hres*127)/5000+
+                        ((header->hres*127)%5000>2499?1:0));                                                //If not equal to vertical, printorizontal resolution
+                        printf("%idpi\n", (header->vres*127)/5000+((header->vres*127)%5000>2499?1:0));      //Print vertical resolution
 
-                        printf("Header size: %u %ix%i %08X:%08X:%08X:%08X (RGBA)\n", header->hsize,header->width,header->height,header->rmsk,header->gmsk,header->bmsk,header->amsk);
 
-                        free(buffer);                                               //Free buffer
-                        return 0;                                                   //Exit - 0
+                        free(buffer);                                                                       //Free buffer
+                        return 0;                                                                           //Exit - 0
                     };
                 }else{fclose(input);printf("Can\'t read \'%s\'\n", argv[1]);};                              //Close file and error out
                 free(buffer);                                                                               //Free buffer
