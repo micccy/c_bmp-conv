3268013987bfde42f901473463caf92965f06945
micccy <michele.giacomone@live.com>
2023-02-23 23:12:23 +0100

    Update library

--- src/deconv.c
+++ src/deconv.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include "lib/struct.h"
+#include "lib/def.h"
 
 uint8_t *buffer;                                                            //Buffer to hold input data
 char *dump;                                                                 //Buffer to hold output data
@@ -15,10 +15,6 @@ struct c_def *head,**seek;                                                  //Li
 
 FILE *input,*output;                                                        //IO stream pointers
 uint32_t insize;                                                            //File size
-struct s_def prsiz;                                                         //Printable file size
-const char magind[][4]={"B\0\0", "KiB", "MiB", "GiB"};                      //Text to display for each magnitude
-
-uint32_t t0,t1;                                                             //tmp
 
 int main(int argc,char*argv[]){
     if((argc==2)||(argc==3)){
@@ -38,36 +34,7 @@ int main(int argc,char*argv[]){
                     (((header->compr==BI_RGB)&&(header->hsize>=24))||((header->compr==BI_BITFIELDS)&&
                     (header->hsize>=52)))&&((header->bpp==1)||(header->bpp==2)||(header->bpp==4)||
                     (header->bpp==8)||(header->bpp==16)||(header->bpp==24)||(header->bpp==32))){            //Check if a sane file has been opened
-                        prsiz.mag=0;
-                        t0=header->fsize;
-                        while(t0>1023){t0/=1024;prsiz.mag++;};
-                        prsiz.value=t0;
-                        printf("\'%s\' %u%s, ",argv[1], prsiz.value, magind[prsiz.mag]);                    //Print name and size of opened file
-                        printf("%ix%i, %ubpp, ",header->width, header->height, header->bpp);                //Print dimensions and bits per pixel
-                        if(header->compr){                                                                  //If bitfields are specified, print them with RGBAX syntax
-                            t0=0;
-                            t1=32;
-                            for(int i=0;i<32;i++) t0+=((header->rmsk&(1<<i))?1:0);
-                            printf("%u.", t0);
-                            t1-=t0;
-                            t0=0;
-                            for(int i=0;i<32;i++) t0+=((header->gmsk&(1<<i))?1:0);
-                            printf("%u.", t0);
-                            t1-=t0;
-                            t0=0;
-                            for(int i=0;i<32;i++) t0+=((header->bmsk&(1<<i))?1:0);
-                            printf("%u.", t0);
-                            t1-=t0;
-                            t0=0;
-                            for(int i=0;i<32;i++) t0+=((header->amsk&(1<<i))?1:0);
-                            t1-=t0;
-                            printf("%u.%u, ", t0,t1);
-                        }
-                        if(header->ncol) printf("%ucol, ", header->ncol);                                   //If not inferred from the bpp, print the number of colors
-                        if(header->hres!=header->vres) printf("%ix", (header->hres*127)/5000+
-                        ((header->hres*127)%5000>2499?1:0));                                                //If not equal to vertical, printorizontal resolution
-                        printf("%idpi\n", (header->vres*127)/5000+((header->vres*127)%5000>2499?1:0));      //Print vertical resolution
-
+                        printheader(header,argv[1]);                                                                //Dump header
                         //Parse file to find number of colors and create palette
                         colnum=0;
 
@@ -76,6 +43,8 @@ int main(int argc,char*argv[]){
                         if(argc==3) output=fopen(argv[2],"w");                                              //Open output file if it exists
                         else output=stdout;
                         if(output){
+
+                        //Write output file
 
                             if(argc==3) fclose(output);                                                     //Close output file
                             free(buffer);                                                                   //Free buffer
@@ -90,7 +59,7 @@ int main(int argc,char*argv[]){
         }else printf("Can\'t open \'%s\'\n", argv[1]);
     }else printf("Invalid arguments\n");
 
-    printf("%s", helpstr);                                                                                  //Print help
+    printhelp(argv[0]);                                                                                     //Print help
 
     return 1;                                                                                               //Exit - 1
 }
--- src/lib/def.h
+++ src/lib/def.h
@@ -43,15 +43,52 @@ struct __attribute((__packed__)) s_def{
     uint16_t value : 10;
 };
 
-const char helpstr[]={                                                      //Help text
-    "This tool can convert a bitmap image in a text file based format\n\
+void printhelp(char *filename){                                                      //Help text
+    printf("\n\
+    This tool can convert a bitmap image in a text file based format\n\
     which can then be used as a seed to generate other images\n\n\
-        usage: deconv input_file [output_file]\n\n\
+        usage: %s input_file [output_file]\n\n\
     The input file must be a (small) bitmap image file having at most\n\
     95 colors and at least a BITMAPINFOHEADER, if more colors are\n\
     present they will be united by similarity to reduce them\n\n\
     The output file will be a simple text delimited by \'#\' where\n\
     each character identifies one pixel\'s color and a palette\n\
     will then be provided below in RGBA format\n\
-    This is meant to be used in making small retro icons etc\n\n"
+    This is meant to be used in making small retro icons etc\n\n", filename);
 };
+
+void printheader(struct h_def *h,char *filename){
+    struct s_def prsiz;                                                                 //Printable file size
+    const char magind[][4]={"B\0\0", "KiB", "MiB", "GiB"};                              //Text to display for each magnitude
+    uint32_t t0,t1;                                                                     //tmp
+
+    prsiz.mag=0;
+    t0=h->fsize;
+    while(t0>1023){t0/=1024;prsiz.mag++;};
+    prsiz.value=t0;
+    printf("\'%s\' %u%s, ",filename, prsiz.value, magind[prsiz.mag]);                    //Print name and size of opened file
+    printf("%ix%i, %ubpp, ",h->width, h->height, h->bpp);                //Print dimensions and bits per pixel
+    if(h->compr){                                                                  //If bitfields are specified, print them with RGBAX syntax
+        t0=0;
+        t1=32;
+        for(int i=0;i<32;i++) t0+=((h->rmsk&(1<<i))?1:0);
+        printf("%u.", t0);
+        t1-=t0;
+        t0=0;
+        for(int i=0;i<32;i++) t0+=((h->gmsk&(1<<i))?1:0);
+        printf("%u.", t0);
+        t1-=t0;
+        t0=0;
+        for(int i=0;i<32;i++) t0+=((h->bmsk&(1<<i))?1:0);
+        printf("%u.", t0);
+        t1-=t0;
+        t0=0;
+        for(int i=0;i<32;i++) t0+=((h->amsk&(1<<i))?1:0);
+        t1-=t0;
+        printf("%u.%u, ", t0,t1);
+    };
+    if(h->ncol) printf("%ucol, ", h->ncol);                                   //If not inferred from the bpp, print the number of colors
+    if(h->hres!=h->vres) printf("%ix", (h->hres*127)/5000+
+    ((h->hres*127)%5000>2499?1:0));                                                //If not equal to vertical, printorizontal resolution
+    printf("%idpi\n", (h->vres*127)/5000+((h->vres*127)%5000>2499?1:0));      //Print vertical resolution
+}
