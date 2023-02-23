f288b1f124415f6957b02671738042fad861ea36
micccy <michele.giacomone@live.com>
2023-02-23 11:30:07 +0100

    Started deconv

--- deconv.c
+++ deconv.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
 
+uint8_t *b;
+
+FILE *input;
+long insize;
+
 int main(int argc,char*argv[]){
-    return 0;
+    if(argc==2){
+        if((input=fopen(argv[1],"rb"))){                                    //Open input file
+            fseek(input,0,SEEK_END);
+            insize=ftell(input);                                            //Get file size
+            fseek(input,0,SEEK_SET);
+            b=malloc(insize);                                               //Allocate big enough buffer
+            if(insize==fread(b,1,insize,input)){                            //Read file into buffer
+                fclose(input);                                              //Close file
+
+                free(b);                                                    //Free buffer
+                return 0;                                                   //Exit - 0
+            }else{fclose(input);printf("Can\'t read \'%s\'\n", argv[1]);};  //Close file and error out
+            free(b);                                                        //Free buffer
+        }else printf("Can\'t open \'%s\'\n", argv[1]);
+    }else printf("Invalid arguments\n");
+
+    printf("HELP (TODO)\n");                                                //Print help
+
+    return 1;                                                               //Exit - 1
 }
