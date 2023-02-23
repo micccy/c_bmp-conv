0c14c9e8943439dc383b19f2bf2df9bcaac82172
micccy <michele.giacomone@live.com>
2023-02-23 12:09:00 +0100

    added headers

--- deconv.c
+++ deconv.c
@@ -2,7 +2,16 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-uint8_t *b;
+uint8_t *buffer;
+
+struct __attribute((__packed__))__{
+    char signature[2];                                                      //"BM"
+    uint32_t size;                                                          //File size
+    uint16_t reserved[2];                                                   //Reserved
+    uint32_t offset;                                                        //Offset of pixel data
+} *header;
+
+uint8_t *pix;
 
 FILE *input;
 long insize;
@@ -13,14 +22,16 @@ int main(int argc,char*argv[]){
             fseek(input,0,SEEK_END);
             insize=ftell(input);                                            //Get file size
             fseek(input,0,SEEK_SET);
-            b=malloc(insize);                                               //Allocate big enough buffer
-            if(insize==fread(b,1,insize,input)){                            //Read file into buffer
+            buffer=malloc(insize);                                          //Allocate big enough buffer
+            if(insize==fread(buffer,1,insize,input)){                       //Read file into buffer
                 fclose(input);                                              //Close file
 
-                free(b);                                                    //Free buffer
+
+
+                free(buffer);                                               //Free buffer
                 return 0;                                                   //Exit - 0
             }else{fclose(input);printf("Can\'t read \'%s\'\n", argv[1]);};  //Close file and error out
-            free(b);                                                        //Free buffer
+            free(buffer);                                                   //Free buffer
         }else printf("Can\'t open \'%s\'\n", argv[1]);
     }else printf("Invalid arguments\n");
 
